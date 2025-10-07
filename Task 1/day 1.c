7fc99d355193064f00db547407f615447adc2648
Om Bagal <ombagal8@gmail.com>
2025-10-07 22:01:55 +0530

    Add files via upload

---
+++ Task 1/day 1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+void main ()
+{
+	printf("Name: Om Bagal\n");
+	printf("Age:21\n");
+	printf("Collage name: G h Raisoni Collage\n");
+    
+}
---
+++ Task 2/Task.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+
+	printf(" --------  \n");
+	printf("|         |\n");
+    printf("|         |\n");
+	printf("|   OM    |\n");
+	printf("|         |\n");
+    printf("|         |\n");
+	printf(" --------  \n");
+	printf("___________________________________\n");
+	
+	printf("  -----  \n");
+	printf("|       |\n");
+    printf("O       |\n");
+	printf("M       |\n");
+	printf("|       |\n");
+    printf("|       |\n");
+	printf("  -----  \n");
+	printf("___________________________________\n");
+	
+    printf("*\n");
+    printf("* *\n");
+    printf("* *\n");
+    printf("* * *\n");
+    printf("* *\n");
+    printf("*\n");
+    printf("___________________________________\n");
+    
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*                                    *  *\n");
+    printf("*                                  *      *\n");
+    printf("*         *   *                  *\n");
+    printf("*      *         *              *\n");
+    printf("*   *              *          *\n");
+    printf("*                     *   *\n");
+}
