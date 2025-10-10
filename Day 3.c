a85296973ab3b88b30f5e3aa0ea68793d607e60d
Om Bagal <ombagal8@gmail.com>
2025-10-10 18:03:53 +0530

    Update Day 3.c

--- Day 3.c
+++ Day 3.c
@@ -12,3 +12,18 @@ void main ()
 	printf("%d",a);
 	
 }
+
+#include <stdio.h>
+#include <conio.h>
+
+void main ()
+{
+	int a=10;
+//	int b=;
+//	a=a+2;
+//  a=a-2; 
+//	a=a-a;
+     a-=2;
+	printf("%d",a);
+	
+}
