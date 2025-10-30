824a763e0c39128e035461209fe0f83450ba7bdb
Om Bagal <ombagal8@gmail.com>
2025-10-30 17:16:56 +0530

    Add files via upload

---
+++ Task1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+ void main()
+ {
+ 	int a, b;
+ 	printf("enter a");
+ 	scanf("%d", &a);
+ 	
+ 	printf("enter b");
+ 	scanf("%d", &b);
+ 	
+ 	printf("sum is : %d", a*b);
+ }
